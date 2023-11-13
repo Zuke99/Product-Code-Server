7e0c7b9bee8801e3a19d09a1600b888bd10c13cb
Dheeraj P <dheerajp@Dheerajs-MacBook-Pro.local>
2023-11-13 18:48:06 +0530

    k

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose =  require("mongoose");
 mongoose.connect("mongodb+srv://dheerajpinninti5:oyeIY0Imv3J6MtuN@cluster0.tznjhyn.mongodb.net/")
 .then(() => {
-    console.log("Connection is Successful");
+    console.log("Connection is Successful!!");
 }).catch((e) => {
-    console.log("Connection Couldn't be Established");
+    console.log("Connection Couldn't be Established!!");
 })

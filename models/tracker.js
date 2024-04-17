804081e95443c127a08ab606de1740e09a6f8350
Dheeraj P <dheerajp@Dheerajs-MacBook-Pro.local>
2024-04-18 00:55:19 +0530

    sl_no

--- models/tracker.js
+++ models/tracker.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const trackerSchema = new mongoose.Schema({
     sl_no : Number,
     fy1 : Number,
-    fy2 : Number
+    fy2 : Number,
+    updated_year: Number
 })
 
 module.exports = mongoose.model("Tracker", trackerSchema);
